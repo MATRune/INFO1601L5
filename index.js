c4db7ff2b5c3ffff17902318e0be5fcfe947f3a6
MATRune <malik.thomas.200531@gmail.com>
2025-03-07 19:59:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,111 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+/*let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
 
 for(let i=0; i< arr.length; i++){
   console.log(arr[i])
+}*/
+
+/*Task 2
+let radius = 7;
+const pi = 3.14;
+
+let area = radius * radius * pi;
+*/
+
+/* Task 3
+let name = "bob";
+let age = 24;
+
+console.log(typeof(name))//string
+console.log(typeof(age))//number
+
+console.log(`Hello my name is ${name}, I'm ${age} years old`);
+//Hello my name is bob, I'm 24 years old.
+
+console.log(`I was born in ${2020 - age}`)
+//I was born in 1966
+*/
+
+/* Task 4.1 and 4.2
+console.log('1' == 1); //true
+console.log(1 == true); //true
+console.log("false" == false); // "false" is not false
+console.log("false" == true); // "false" is not true either
+
+if("false")
+   console.log("Hello false!") //but "false" is true enough here
+
+console.log('1' === 1); //false
+console.log(1 === true); //false
+console.log("true" === true); //false
+*/
+
+/* Task 5
+function happyPrint(string){
+ console.log("😀: "+string);
 }
+
+function sadPrint(string){
+ console.log("😢: "+string);
+}
+
+//This high-order function adds 2 parameters and passes the answer to the callback
+function add(a, b, callback){
+   let ans = a + b;
+   callback(ans);// call the callback and pass the answer to it
+}
+
+//call add passing the callbacks to it
+
+add(5, 10, happyPrint);
+add(11, 12, sadPrint);
+*/
+
+/* Task 6
+function printDate(){
+   console.log(Date().toLocaleTimeString());
+}
+      
+setInterval(printDate, 1000);
+*/
+
+/* Task 5.2
+
+let arr = [12, 33, 4, 5, -4, 8, 19, 25];
+                
+//map() creates a new array from the elements of one without changing the old one
+function double(num){
+  return num * 2;
+}
+
+let doubledArr = arr.map(double); 
+console.log(doubledArr);
+
+function isOdd(num){
+  return element%2 !== 0; 
+}
+//Filter takes a test condition and returns only the element which 
+//make the condition true
+let odds = arr.filter(isOdd);
+console.log(odds);
+
+//Returns true or false if any of the elements of the array 
+//meets a specified condition
+function has5Factor(){
+  return ele % 5 === 0;
+}
+
+let hasFiveFactor = arr.some(has5Factor);
+console.log(hasFiveFactor);
+
+function intCompare(a, b){
+ return a - b;
+}
+
+//sort function must return either 0, +ve, -ve
+let ascending = arr.sort(intCompare)
+console.log(ascending);
+*/
+
+/* Exercise
+
+*/
